47c1935535fcfa91744dceccf6137f3b0c10f838
Hewitt02M <masakurehewitt21@.com>
2024-06-01 12:05:34 -0700

    web-01

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using puppet to make changes
+
+file { 'etc/ssh/ssh_config':
+        ensure => present,
+
+content =>"
+
+        #SSH client configuration
+        host*
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+}
